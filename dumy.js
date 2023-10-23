7df1e21a6319528c1601707dd8cb3b8e8eb20974
Muhammad-Gulraiz <140408290+Muhammad-Gulraiz@users.noreply.github.com>
2023-10-23 22:17:03 +0500

    dumy.js

---
+++ dumy.js
@@ -0,0 +1,635 @@
+"use strict";
+// let name1:string = "Gulraiz";
+// let name1:number = 123;
+// let name1:boolean = true;
+// console.log(name1);
+// function pakistan(){
+//     console.log("I love Pakistan");
+// }
+// pakistan();
+// function Qadoos(){
+//     console.log("He is my sweet son.");
+// }
+// Qadoos();
+// function jada(){
+//     return  "Jhelum"
+// }
+// let name1:string = jada()
+// console.log(name1);
+// function Ashir(){
+//     return 123
+// }
+// let x:number = Ashir();
+// console.log(x);
+// function Rose(){
+//     return "I love Pakistan"
+// }
+// let x:string = Rose();
+// console.log(x);
+// function sundus(){
+//     return 740163
+// }
+// let s:number = sundus();
+// console.log(s);
+// function England(){
+//     console.log("Ab kay hum bicharay to shaid ");
+// }
+// England();
+// function school(){
+//     return 2961
+// }
+// let y:number = school();
+// console.log(y);
+// function Gulraiz(){
+//     console.log(123456);
+// }
+// Gulraiz();
+// function fruit(){
+//     return "oil"
+// }
+// let Pakistan:string = fruit();
+// console.log(Pakistan);
+// function train(){
+//     console.log("teachers");
+// }
+// train();
+// function tv(){
+//     return "ball"
+// }
+// let x:string = tv();
+// console.log(x);
+// function sum(){
+//     let total = 2+2;
+//     return total
+// }
+// let s:number = sum();
+// console.log(s);
+// function multiply(){
+//     let product = 6*9;
+//     return product
+// }
+// let s:number = multiply();
+// console.log(s);
+// function tree(){
+//     let division = 9/6;
+//     return division
+// }
+// let x:number= tree();
+// console.log(x);
+// function singer(){
+//     let subtraction = 9-3;
+//     return subtraction;
+// }
+// let x:number= singer();
+// console.log(x);
+// function calculator(){
+//     console.log(2881);
+// }
+// calculator();
+// function air(){
+//     return "Ali Idrees"
+// }
+// let x:string = air();
+// console.log(x);
+// Arrow-function
+// function arrowfuntion(){
+//     console.log("Gulraiz");
+// }
+// arrowfuntion();
+// function arrowfuntion(){
+//     let s:number= 6523;
+//     return s
+// }
+// console.log(arrowfuntion());
+// function arrowfuntion(){
+//     let sum: number = 6+3;
+//     return sum;
+// }
+// let x:number = arrowfuntion();
+// console.log(x);
+// function arrowfuntion(){
+//     console.log("High way to heaven")
+// }
+// arrowfuntion();
+// arrowfuntion();
+// arrowfuntion();
+// arrowfuntion();
+// function arrowfuntion(){
+//     console.log("very sweet");
+// }
+// function anotherfunction(){
+//     arrowfuntion()
+// }
+// anotherfunction()
+// function secondfunction(){
+//     console.log(740163);
+// }
+// function anotherfunction(){
+//     secondfunction()
+// }
+// anotherfunction()
+// anotherfunction()
+// anotherfunction()
+// let newfunction = ()=>{
+//     return 6/3
+// }
+// let x:number = newfunction()
+// console.log(x)
+// let ali = {
+//      firstname:"sweet",
+//     lastname:"heart",
+//     age: 12,
+//     hobbies:["aa", "bb", "CC"]
+//     salary:()=>  "month",
+//     email:"kkss"
+// }
+// delete ali.salary 
+// console.log(ali)
+// let ali = {
+//     firstname:"sweet",
+//    lastname:"heart",
+//    age: 12,
+//    hobbies:["aa", "bb", "CC"]
+//    salary:()=>  "month",
+//    email:"mgulraiz00@gmail.com"
+// }
+// delete ali.salary 
+// ali.adress = "house#306.NR masjid mochian jada jhelum",
+// console.log(ali)
+// let x:number = Math.round(10.3);
+// console.log(x);
+// interface Person {
+//     name:string;
+//     age:number;
+//     subject:string
+// }
+// interface employee extends Person {
+//     id:number
+// }
+// interface shopkeeper extends employee {
+//     adress:string
+// }
+// let person:person = {
+//     name:"Ali",
+//     age:19,
+//     subject:"English"
+// }
+// let employee:employee = {
+//     name:"Rashid",
+//     age: 18,
+//     id:740163,
+// }
+// let shopkeeper:shopkeeper = {
+//     name:"Aslam",
+//     age: 47,
+//     adress:"jada jhelum"
+// }
+// console.log(employee.id)
+// interface person {
+//     id:number
+// }
+// interface person {
+//     adress:string
+// }
+// let person = {
+//     id: 753,
+//     adress: "mehmoodabad"
+// }
+// console.log(person.id)
+// class student{
+//     name:string
+//     age:number
+//     profession:string
+//     rollnumber:number
+//     constructor(n:string,a:number,p:string,r:number){
+//         this.name=n
+//         this.age=a
+//         this.profession=p
+//         this.rollnumber=r
+//     }
+// }
+// class medical{
+//     name:string;
+//     age:number;
+//     qualification:string;
+//     id:string;
+//     service:string;
+//     adress: string
+//     constructor(n:string,a:number,q:string,i:string,s:string,b:string){
+//         this.name=n
+//         this.age=a
+//         this.qualification=q
+//         this.id=i
+//         this.service=s
+//         this.adress=b
+//     }
+//     medicalFunc():string{
+//         return 'medical name is ${this.name}, and his age is ${this.age}, and his qualification is ${this.qualification}, and his id is ${this.id}, and his service is ${this.service}, and his adress is ${this.adress}'
+//     }       
+// }
+// const medical1 = new medical("Qadoos",12,"MBBS","mountain","16years","jada jhelum")
+// const medical2 = new medical("AAsshir",13,"DHMS","house","19years","mehmoodabad jhelum")
+// const medical3 = new medical("Gulraiz",100,"scientist","river","18years","daryala jhelum")
+// console.log(medical1,medical2,medical3)
+// class medical{
+//     name:string;
+//     age:number;
+//     qualification:string;
+//     id:string;
+//     service:string;
+//     adress: string
+//     constructor(n:string,a:number,q:string,i:string,s:string,b:string){
+//         this.name=n
+//         this.age=a
+//         this.qualification=q
+//         this.id=i
+//         this.service=s
+//         this.adress=b
+//     }
+//     medicalFunc():string{
+//         return 'medical name is ${this.name}, and his age is ${this.age}, and his qualification is ${this.qualification}, and his id is ${this.id}, and his service is ${this.service}, and his adress is ${this.adress}'
+//     }       
+// }
+// const medical1 = new medical("Qadoos",12,"MBBS","mountain","16years","jada jhelum")
+// const medical2 = new medical("AAsshir",13,"DHMS","house","19years","mehmoodabad jhelum")
+// const medical3 = new medical("Gulraiz",100,"scientist","river","18years","daryala jhelum")
+// let medicaldata:medical[] = []
+// medicaldata.push(medical1)
+// medicaldata.push(medical2)
+// medicaldata.push(medical3)
+// console.log(medicaldata)
+// class medical{
+//     name:string;
+//     age:number;
+//     qualification:string;
+//     id:string;
+//     service:string;
+//     adress: string
+//     constructor(n:string,a:number,q:string,i:string,s:string,b:string){
+//         this.name=n
+//         this.age=a
+//         this.qualification=q
+//         this.id=i
+//         this.service=s
+//         this.adress=b
+//     }
+//     medicalFunc():string{
+//         return 'medical name is ${this.name}, and his age is ${this.age}, and his qualification is ${this.qualification}, and his id is ${this.id}, and his service is ${this.service}, and his adress is ${this.adress}'
+//     }       
+// }
+// const medical1 = new medical("Qadoos",12,"MBBS","mountain","16years","jada jhelum")
+// const medical2 = new medical("AAsshir",13,"DHMS","house","19years","mehmoodabad jhelum")
+// const medical3 = new medical("Maqadas",100,"scientist","river","18years","daryala jhelum")
+// let medicaldata:medical[] = []
+// medicaldata.push(medical1)
+// medicaldata.push(medical2)
+// medicaldata.push(medical3)
+// medical3.name = "sundus"
+// console.log(medicaldata)
+// class jada{
+//     name:string;
+//     age:number
+//     constructor(n:string,a:number){
+//         this.name=n
+//         this.age=a
+//     }
+//     jadaFunc():string{
+//         return 'jada name is ${this.name}, his age is ${this.age}'
+//     }
+// }
+// const jada1 = new jada("Bushra",12)
+// const jada2 = new jada("imran khan",25)
+// const jada3 = new jada("sweet",88 )
+// console.log(jada1,jada2,jada3)
+// class student{
+//     name:string;
+//     age:number;
+//     rollnumber:number;
+//     hobbies:string
+//     constructor(n:string,a:number,r:number,h:string){
+//         this.name=n
+//         this.age=a
+//         this.rollnumber=r
+//         this.hobbies=h
+//     }
+//     studentFunc():string{
+//         return 'student name is ${this.name}, and student age is ${this.age}, and rollnumber is ${this.rollnumber}, and hobbies are ${this.hobbies}'
+//     }
+// }
+// const student1 = new student("Archemidas",12,511,"cricket")
+// const student2 = new student("Harlc", 22, 789, "footbal")
+// const student3 = new student("pakistan", 55, 111, "Hockey")
+// let x:student[]=[]
+// x.push(student1)
+// x.push(student2)
+// x.push(student3)
+// console.log(x)
+// enum students {
+//     Ali,Bilal,Qadoos,AAsshir,school
+// }
+// const x:student= students.Qadoos
+// console.log(x)
+// enum pakistan {
+//     pujab, sindh, peshawar, koita, murree
+// }
+// const x:pakistan= pakistan.sindh
+// const y: string = pakistan[4]
+// // console.log(y)
+// console.log(x)
+// enum pakistan {
+//     pujab=91, sindh, peshawar, koita, murree
+// }
+// const x:pakistan= pakistan.sindh
+// const y: string = pakistan[95]
+// console.log(y)
+// // console.log(x)
+// // while and do loop
+// let i =1
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+// let i = 1
+// while(i<=20){
+//     console.log(i)
+//     i++
+// }
+// let i = 1
+// do{
+//     console.log(i)
+// }
+// while(i<=0)
+// ===========================================================================
+// enum calculator {
+//     sum, subtraction, multiply, division
+// }
+// const x:calculator= calculator.sum
+// // const y: string = pakistan[4]
+// // // console.log(y)
+// console.log(x)
+// function tree(){
+//         let division = 9/6;
+//         return division
+//     }
+//     let x:number= tree();
+//     console.log(x);
+// ================================================================================
+// if else:
+// if(false){
+//     console.log("Welcome")
+// }
+// else{
+//     console.log("Goodbye")
+// }
+// let username:string = "muhammadgulraiz"
+// let password:number = 740163
+// if(username== "muhammadgulraiz" && password== 740163 ){
+//     console.log("Welcom to my home")
+// }
+// else{
+//     console.log("Good bye")
+// }
+// =============================================================================
+// class house{
+//     name:string
+//     age:number
+//     constructor(name:string,age:number){
+//         this.name=name
+//         this.age=age                       
+//     }
+//     studentFunc(){
+//         return 'house name is ${this.name}, his age is ${this.age}'
+//     }      
+// }
+// const house1 = new house("Qadoos", 123)
+// console.log(house1)
+// =================================================
+//     class calculator{
+//         answer: number= 0
+//         add(n1:number,n2:number):number{
+//             return this.answer = n1 +n2
+//         }
+//         sub(n1:number,n2:number):number{
+//             return this.answer = n1 - n2
+//         }
+//         mul(n1:number,n2:number):number{
+//             return this.answer = n1 * n2
+//         }
+//         div(n1:number,n2:number):number{
+//             return this.answer = n1 / n2
+//         }
+//         tan(n1:number,n2:number):number{
+//             return this.answer=tan(n1*n2)
+//         }        
+// }
+// const calculator1 = new calculator(0);
+// let x = calculator1.tan(2,15)
+// console.log(x)
+//     class calculator{
+//         n1 : number
+//         n2 : number
+//         answer: number = 0;
+//         constructor(n:number, n2:number, answer:number){
+//             this.n1 = n1
+//             this.n2 = n2 
+//             this.answer = answer 
+//         }             
+//         add(n1:number,n2:number):number{
+//             return this.answer = n1 +n2
+//         }
+//         sub(n1:number,n2:number):number{
+//             return this.answer = n1 - n2
+//         }
+//         mul(n1:number,n2:number):number{
+//             return this.answer = n1 * n2
+//         }
+//         div(n1:number,n2:number):number{
+//             return this.answer = n1 / n2
+//         }
+// }
+// const calculator1 = new calculator(0);
+// calculator1.add(2,3)
+// console.log(calculator1)
+// class calculator{
+//     n: number=0
+//     answer: number 
+//     constructor(n:number){
+//         this.answer = n
+//     }                
+//     tangent(n:number):number{
+//         return this.answer = n
+//     }        
+// }
+// const calculator1 = new calculator(0); 
+// let x = calculator1.tangent(30)
+// console.log(x)
+// ===================================================
+// 30
+// Ternary operator:
+// Condition or conditions ? code 1 : code 2
+// let username = "Aashir"
+// if(username== "Aashir"){
+//     console.log("you are so sweet")
+// }else{
+//     console.log("It is invalid usearname")
+// }
+// Ternary operator:
+// Condition or conditions ? code 1 : code 2
+// let username = "Aashir"
+// username== "Aashir" ? 
+// console.log("you are so sweet") : 
+// console.log("It is invalid usearname")
+// let username = "Aashir"
+// let password = 133
+// username== "Aashir" && password == 123 ? 
+// console.log("you are so sweet") : 
+// console.log("It is invalid usearname")
+// ================================================
+// 25
+// interface in typescript:
+// i.just object can be written:
+// let obj = {
+//     fname : "Ali", 
+//     age:31
+// }
+// console.log(obj)
+// interface faisal{
+//     fname:string;
+//     age:number
+//     func:()=> void;
+//     hobbies:string[];
+//     socialMedia:{
+//         instagram:string;
+//         facebook:string
+//     }
+// }
+// let obj = {
+//     fname : "Ali", 
+//     age:31,
+//     func:()=>{
+//         console.log("hello")
+//     },
+//     hobbies:["cricket", "hockey"],
+//     socialMedia:{
+//         intagram:"jell",
+//         facebook:"@gmail.com"
+//     }
+// }
+// console.log(obj)
+// ========================================================
+// class staff{
+//     name:string;
+//     age:number;
+//     qualification:string;
+//     id:string;
+//     service:string;
+//     adress: string
+//     constructor(name:string,age:number,qualification:string,id:string,service:string,adress:string){
+//         this.name=name
+//         this.age=age
+//         this.qualification= qualification
+//         this.id=id
+//         this.service= service
+//         this.adress= adress
+//     }
+//     schoolstaff():string{
+//         return 'staff name is ${this.name}, and his age is ${this.age}, and his qualification is ${this.qualification}, and his id is ${this.id}, and his service is ${this.service}, and his adress is ${this.adress}'
+//     }       
+// }
+// const staff1 = new staff("Ishtiaq-headmaster",52,"M.A,M.ed","iceland","31years","near:islamia school")
+// const staff2 = new staff("jabran-sst",39,"M.phil,M.ed","house","12years","kalagujran jhelum")
+// const staff3 = new staff("Haroon-est",40,"mcs,b.ed","water","18years","daryala jhelum")
+// // let x:staff[] = []
+// // x.push(staff1)
+// // x.push(staff2)
+// // x.push(staff3)
+// // console.log(x)
+// console.log(staff1,staff2,staff3)
+// ========================================================
+// class studen{
+//     name:string;
+//     age: number;
+//     fatherName: string;
+//     rollNumber: number;
+//     adress: string
+//     constructor(name:string,age:number,fatherName:string,rollNumber:number,adress:string){
+//         this.name=name,
+//         this.age=age,
+//         this.fatherName=fatherName,
+//         this.rollNumber=rollNumber,
+//         this.adress=adress
+//     }
+//     schoolstuden():string{
+//         return 'student name is ${this.name} and student age is ${this.age} and fatherName is ${this.fatherName}'
+//     }
+// }
+// const student1= new student("Hamza",14,"Ali Raza",49, "Lahore")
+// const student2= new student("Zaheer",13,"Zafar Iqbal",50, "Chandra")
+// const student3= new student("Saad",15,"Nisaar ahmed",51, "darapur")
+// const student4= new student("Aoun",16,"Ashraf Ahmed",52, "Tillajogyan")
+// console.log(student1,student2,student3)
+// =======================================================================================
+// class register{
+//     name:string;
+//     page:number;
+//     items: number;
+//     incharge:string
+//     constructor(name:string,page:number,items:number,incharge:string){
+//         this.name=name,
+//         this.page=page,
+//         this.items=items,
+//         this.incharge=incharge
+//     }
+//     schoolregister(){
+//         return 'register name is ${this.name} and pages are ${this.page}'
+//     }
+// }
+// const register1 =new register("admission",100,90, "naveed")
+// const register2 =new register("science lab",100,90, "yasir")
+// const register3 =new register("compute",100,90, "Ahtesham")
+// const register4 =new register("physics", 200, 25, "haider sb")
+// console.log(register1,register2,register3,register4)
+// =================================================================
+// class physicslab{
+//     name:string;
+//     qty:number
+//     constructor(name:string,qty:number){
+//         this.name=name,
+//         this.qty=qty
+//     }
+//     schoollab(){
+//         return 'name of apperatus is ${this.name} and it quantity is ${this.qty}'
+//     }
+// }
+// const physicslab1 = new physicslab("vernier caliper", 10)
+// const physicslab2 = new physicslab("screw guage", 8)
+// const physicslab3 = new physicslab("helicar spring", 10)
+// const physicslab4 = new physicslab("Gravesand Apperatus", 8)
+// const physicslab5 = new physicslab("free fall method", 2)
+// const physicslab6 = new physicslab("glass slabs", 20)
+// const physicslab7 = new physicslab("glass prisms", 20)
+// const physicslab8 = new physicslab("bar magnets", 30)
+// console.log(physicslab1,physicslab2,physicslab3,physicslab4,physicslab5,physicslab6,physicslab7,physicslab8)
+// ====================================================================================
+class rooms {
+    constructor(name, door, qty, windows) {
+        this.name = name,
+            this.door = door,
+            this.qty = qty,
+            this.windows = windows;
+    }
+    clerkroom() {
+        return 'qty of stock instruments are ${this.qty}';
+    }
+    schoolHall() {
+        return 'school doors are ${this.door}';
+    }
+    school() {
+        return 'school rooms are ${this.name} and  windows are{this.windows}';
+    }
+}
+const rooms1 = new rooms("headmaster room", 1, 1, 4);
+const rooms2 = new rooms("computer lab ", 2, 2, 4);
+const rooms3 = new rooms("headmaster room", 2, 1, 4);
+const rooms4 = new rooms("headmaster room", 1, 1, 3);
+console.log(rooms2);
